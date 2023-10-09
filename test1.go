0189732807590a80a8c4bfa5b1bbccf89f7680d5
Bapan Biswas <46675716+babiswas@users.noreply.github.com>
2023-10-09 09:42:29 +0530

    Commit
    Commit

--- test1.go
+++ test1.go
@@ -1,38 +1,78 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
+	"log"
 )
 
-func mygreet(ctx context.Context) <-chan string {
-	ch1 := make(chan string)
-	go func() {
-		for {
-			select {
-			case ch1 <- fmt.Sprintf("Hello %s", "bapan"):
-				fmt.Println("Message Passed.")
-			case <-ctx.Done():
-				if err := ctx.Err(); err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println("Quitting the routine.")
-				return
-			}
-		}
-	}()
-	return ch1
-}
-
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	ch1 := mygreet(ctx)
-	num := 100
-	for i := 0; i < num; i++ {
-		fmt.Println("Message received ", <-ch1)
+	m := make(map[int]bool)
+
+	log.Println("Adding values in set.")
+
+	add := func(num int) {
+		_, ok := m[num]
+		if ok == false {
+			m[num] = true
+		}
 	}
-	cancel()
-	time.Sleep(time.Millisecond * 1000)
-	fmt.Println("Quiting the process.")
+
+	log.Println("Removing values in set.")
+
+	remove := func(num int) {
+		_, ok := m[num]
+		if ok == true {
+			delete(m, num)
+		}
+	}
+
+	log.Println("Does the number exist.")
+
+	is_exist := func(num int) bool {
+		_, ok := m[num]
+		if ok == true {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	m[1] = true
+	m[2] = true
+	m[3] = true
+	m[4] = true
+
+	fmt.Println("============================================")
+	fmt.Println("Map operations:")
+
+	fmt.Println("===========CHECK IF EXIST====================")
+
+	for _, val := range []int{1, 2, 3, 4, 5, 6, 7, 8} {
+		message := fmt.Sprintf("Does the number %d exist", val)
+		log.Println(message)
+		fmt.Println("Does the number exist ", is_exist(val))
+	}
+
+	fmt.Println("================================================")
+
+	fmt.Println("===============INSERT NUMBER====================")
+
+	for _, val := range []int{1, 2, 3, 4, 5, 6, 7, 8} {
+		message := fmt.Sprintf("Insert the number %d", val)
+		log.Println(message)
+		add(val)
+	}
+
+	fmt.Println("================================================")
+
+	fmt.Println("=================REMOVE NUMBER===================")
+
+	for _, val := range []int{1, 2, 3, 4, 5, 6, 7, 8} {
+		message := fmt.Sprintf("Remove the number %d", val)
+		log.Println(message)
+		remove(val)
+	}
+
+	fmt.Println("==================================================")
+
 }
